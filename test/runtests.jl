6671fbea6abb3d6a33c0a3c26c1c582f393858aa
Ravi <ravi.kunjwal@gmail.com>
2018-09-21 13:22:24 -0400

    Initial commit

---
+++ test/runtests.jl
@@ -0,0 +1,12 @@
+using Example2018
+
+#@show Point(1,2) + Point(3,4)
+
+#q=MPoint(1,2)
+#@show q
+#q.x=4
+#@show q
+
+@show Polynomial([1,2,3])
+@show evaluate(Polynomial([1,2,3]),2)
+#@show Polynomial(
